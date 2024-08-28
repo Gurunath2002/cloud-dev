2a44bc5daeb0170b75c3f3a9ea0e860303bd79c1
gurunath <gurunathgoudhepurge@gmail.comgit config --global user.email gurunathgoudhepurge@examole.com>
2024-08-28 15:31:27 +0530

    update day-4

---
+++ day-4 lifecycle/main.tf
@@ -0,0 +1,27 @@
+resource "aws_s3_bucket" "dev" {
+    bucket = "naresh_devops_awsdevops"
+  
+}
+resource "aws_s3_bucket_versioning" "versioning_ecample" {
+    bucket = aws_s3_bucket.dev.id
+    versioning_configuration {
+        status = "Enabled"
+    }
+  
+}
+
+
+#below examples are for lifecycle meta_arguments 
+
+#   lifecycle {
+#     create_before_destroy = true    #this attribute will create the new object first and then destroy the old one
+#   }
+
+# lifecycle {
+#   prevent_destroy = true    #Terraform will error when it attempts to destroy a resource when this is set to true:
+# }
+
+
+#lifecycle {
+    #ignore_changes = [tags,] #This means that Terraform will never update the object but will be able to create or destroy it.
+#}
